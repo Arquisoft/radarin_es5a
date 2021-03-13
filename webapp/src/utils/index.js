ff4a9785e003ded1b4dc22a6bb95d9563f4f0844
Laura <uo265087@uniovi.es>
2021-03-13 23:24:17 +0100

    introducción del pod en la aplicación con la ventana de login

---
+++ webapp/src/utils/index.js
@@ -0,0 +1,27 @@
+import { media } from './styledComponents';
+import { expandedProperty } from './context';
+import { successToaster, errorToaster } from './toaster';
+import * as ldflexHelper from './ldflex-helper';
+import * as notification from './notification';
+import * as storageHelper from './storage';
+import * as permissionHelper from './permissions';
+import * as languageHelper from './language';
+
+function* entries(obj) {
+  for (const key of Object.keys(obj)) {
+    yield [key, obj[key]];
+  }
+}
+
+export {
+  media,
+  expandedProperty,
+  entries,
+  ldflexHelper,
+  storageHelper,
+  successToaster,
+  errorToaster,
+  notification,
+  permissionHelper,
+  languageHelper
+};
