6611df37fc14b2caa60af38e1bb61d00e93a3a42
German <UO258472@uniovi.es>
2021-04-07 19:44:17 +0200

    Arreglo tests

--- webapp/src/components/EmailForm.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render, fireEvent } from "@testing-library/react";
-import EmailForm from "./EmailForm";
-
-test('check that everything is rendering propertly', async () => {
-  const { getByText } = render(<EmailForm/>);
-  expect(getByText("Submit")).toBeInTheDocument();
-});
