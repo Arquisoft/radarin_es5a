8a8f46ce14684ed7bd0f0b53408cc733117fa861
German <UO258472@uniovi.es>
2021-04-08 14:42:56 +0200

    Tests segundo intento

--- restapi/tests/api.test.js
+++ restapi/tests/api.test.js
@@ -1,35 +1,33 @@
 const request = require('supertest');
-const app = require('./server-for-tests')
-const api = require('./../api') 
+const server = require('./server-for-tests')
+
+/**
+ * Connect to a new in-memory database before running any tests.
+ */
+beforeAll(async () => {
+    await server.startdb()
+    app = await server.startserver()
+});
 
 /**
  * Remove and close the db and server.
  */
 afterAll(async () => {
-    await database.off() //finish the DB
+    await server.closeServer() //finish the server
+    await server.closeDB()
 })
 
 /**
  * Product test suite.
  */
-describe('anadirUsuario ', () => {
-    /**
-     * Test that we can list users without any error.
-     */
-    it('can be listed',async () => {
-        const response = await request(app).get("/api/users/list");
-        expect(response.statusCode).toBe(200);
-    });
-
+describe('Anadir Nuevo Usuario ', () => {
     /**
      * Tests that a user can be created through the productService without throwing any errors.
      */
-    it('can be created correctly', async () => {
-        username = 'Pablo'
-        email = 'pablo@uniovi.es'
-        const response = await request(app).post('/api/users/add').send({name: username,email: email}).set('Accept', 'application/json')
+    it('enviar datos a Api', async () => {
+        nombre = 'Pablo'
+        ubicacion = 'Oviedo'
+        const response = await request(app).post('/users/update').send({name: nombre,ubicacion: ubicacion}).set('Accept', 'application/json')
         expect(response.statusCode).toBe(200);
-        expect(response.body.name).toBe(username);
-        expect(response.body.email).toBe(email);
     });
 });
--- restapi/tests/server-for-tests.js
+++ restapi/tests/server-for-tests.js
@@ -1,43 +1,49 @@
+/*
+This server file allow to start the restapi using an in-memory database
+This will be handy for testing
+*/
+
 const express = require("express")
-const promBundle = require("express-prom-bundle");
 const cors = require('cors');
-
-const app = express()
-
-//Firebase
+const api = require("../api")
 var firebase = require("firebase");
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAq2HShQb--f_SyBYJ6k8RzbKEaJtCvRqM",
-    authDomain: "radarines5a-3b110.firebaseapp.com",
-    databaseURL: "https://radarines5a-3b110-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "radarines5a-3b110",
-    storageBucket: "radarines5a-3b110.appspot.com",
-    messagingSenderId: "176384766915",
-    appId: "1:176384766915:web:0fe6ccda05cb18c9384376"
-  };
-  
-firebase.initializeApp(firebaseConfig);
-console.log(firebase.database());
-var database = firebase.database();
 
+module.exports.startdb = async () => {
+    var firebaseConfig = {
+        apiKey: "AIzaSyAq2HShQb--f_SyBYJ6k8RzbKEaJtCvRqM",
+        authDomain: "radarines5a-3b110.firebaseapp.com",
+        databaseURL: "https://radarines5a-3b110-default-rtdb.europe-west1.firebasedatabase.app",
+        projectId: "radarines5a-3b110",
+        storageBucket: "radarines5a-3b110.appspot.com",
+        messagingSenderId: "176384766915",
+        appId: "1:176384766915:web:0fe6ccda05cb18c9384376"
+    };
 
-//Monitoring middleware
-const metricsMiddleware = promBundle({includeMethod: true});
-app.use(metricsMiddleware);
+    firebase.initializeApp(firebaseConfig);
+    console.log(firebase.database());
+}
 
-app.use(cors());
-app.options('*', cors());
-app.use(express.json())
+module.exports.startserver = async () => {
 
-require("./routes/ubicaciones.js")(app, firebase);
+    app = express()
 
+    app.use(cors());
+    app.options('*', cors());
+    app.use(express.json())
 
-app.listen(process.env.PORT || 5000, () => {
-    console.log("Server has started")
-})
+    require("./../routes/ubicaciones.js")(app, firebase);
 
-export const app = app;
+    server = await app.listen(5000)
+    console.log("Server has started!")
+    return app
+}
 
+module.exports.closeServer = async () => {
+    await server.close()
+}
 
+module.exports.closeDB = async () => {
+    await firebase.database().off();
+}
 
