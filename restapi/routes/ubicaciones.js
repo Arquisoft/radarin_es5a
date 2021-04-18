ce2b374244d8e3780cb1a905f08d02bd4d59bd77
German <UO258472@uniovi.es>
2021-04-18 11:35:23 +0200

    Rubicaciones

--- restapi/routes/ubicaciones.js
+++ restapi/routes/ubicaciones.js
@@ -1,72 +1,63 @@
 module.exports = function (app, firebase){
     
-    // Refresh ubication
-    app.post("/users/update", function (req, res) {
-    firebase.database().ref('users/' + req.body.name).set({
+  // Refresh ubication
+  app.post("/users/update", function (req, res) {
+  firebase.database().ref('users/' + req.body.name).set({
+      
+      ubicacion: req.body.ubicacion
+    });
+  res.send("hola")
+  })
+
+  // Anadir usuarios test
+  app.post("/test/update", function (req, res) {
+    firebase.database().ref('test/' + req.body.name).set({
         
         ubicacion: req.body.ubicacion
       });
-    res.send("hola")
+    res.send("hola test")
     })
 
-    // Anadir usuarios test
-    app.post("/test/update", function (req, res) {
-      firebase.database().ref('test/' + req.body.name).set({
-          
-          ubicacion: req.body.ubicacion
+  app.get("/users/getByIdp", function (req, res) {
+    var lat;
+    var lng;
+    var webId;
+      firebase.database().ref('users/uo258472-solid').get().then(function(snapshot) {
+          if (snapshot.exists()) {
+            lat = snapshot.val().lat;
+            lng = snapshot.val().lng;
+            webId = snapshot.val().webId;
+            console.log(lat);
+            console.log(lng);
+            console.log(webId);
+          }
+          else {
+            console.log("No data available");
+          }
+        }).catch(function(error) {
+          console.error(error);
         });
-      res.send("hola test")
+      res.send("hola")
       })
+
+
+  const SolidNodeClient = require('solid-node-client').SolidNodeClient;
+  const client = new SolidNodeClient();
+
+  app.post("/users/login", async function (req, res)  {
+    try {
+        let session = await client.login({
+            idp: req.body.idp,
+            username: req.body.username,
+            password: req.body.password
+        });
+        res.status(200);
+        res.send(session.webId);
+        console.log(session.webId);
+    } catch (err) {
+        res.status(403);
+        res.send(err);
+    }
+});
+
 }
-    app.get("/users/getByIdp", function (req, res) {
-        firebase.database().ref('users/daniel').get().then(function(snapshot) {
-            if (snapshot.exists()) {
-              console.log(snapshot.val());
-            }
-            else {
-              console.log("No data available");
-            }
-          }).catch(function(error) {
-            console.error(error);
-          });
-        res.send("hola")
-        })
-
-
-    // app.get("/users/getByName", function (req, res) {
-    //     var ubi;
-    //     var key;
-    //     var childKey ;
-    //     firebase.database().ref('users/').once('value').then(function(snapshot) {
-    //         key = snapshot.val(); // "ada"
-    //         childKey = snapshot.child("daniel/ubication").val(); // "last"
-    //       });
-    //     console.log("ubicacion: " + key+childKey);
-    //     res.send("ok")
-    //     })
-    
-
-
-
-    const SolidNodeClient = require('solid-node-client').SolidNodeClient;
-    const client = new SolidNodeClient();
-
-    app.post("/users/login", async function (req, res)  {
-      try {
-          let session = await client.login({
-              idp: req.body.idp,
-              username: req.body.username,
-              password: req.body.password
-          });
-          res.status(200);
-          res.send(session.webId);
-          console.log(session.webId);
-      } catch (err) {
-          res.status(403);
-          res.send(err);
-      }
-  });
-  
-}
-
-
