3f10ddc64d5448f1c049bb981b919c7077e5dccd
German <UO258472@uniovi.es>
2021-04-29 16:08:47 +0200

    Para CI

---
+++ webapp/jest-cucumber.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  projects: ['radarin_es5a/webapp'],
+  verbose: true,
+  resolver: "jest-pnp-resolver",
+  preset: 'jest-puppeteer',
+  testRegex: '(/feature/.*|(\\.|/)(feature))\\.[jt]sx?$',
+  testTimeout: 30000,
+  setupFiles: [
+    "react-app-polyfill/jsdom"
+  ],
+  "transform": {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^(?!.*\\.(js|jsx|ts|tsx|json)$)": "<rootDir>/config/jest/fileTransform.js"
+  },
+  "transformIgnorePatterns": [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
+    "^.+\\.module\\.(css|sass|scss)$"
+  ],
+  "moduleNameMapper": {
+    "^react-native$": "react-native-web",
+    "\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules",
+    "@inrupt/solid-style-guide": "<rootDir>/node_modules/jest-css-modules"
+  }
+};
